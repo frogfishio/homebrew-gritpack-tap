edad979ddc7da58a3a0f8af81b3d086b626d04d7
eldiablolives <eldiablolives@live.com>
2026-09-15 07:32:28 +0700

    Publish the 1.2.10 macOS recovery formula

--- Formula/gritpack.rb
+++ Formula/gritpack.rb
@@ -1,9 +1,9 @@
 class Gritpack < Formula
   desc "Multi-language package manager and package hub"
   homepage "https://frogfish.io"
-  url "https://github.com/frogfishio/homebrew-gritpack/releases/download/v1.2.6/gritpack-1.2.6-aarch64-apple-darwin.tar.gz"
-  version "1.2.6"
-  sha256 "252c3af6bb3bfacfb179d2bdfe7b1eb92cfa17ab9cf91741e79df16bebd6616c"
+  url "https://github.com/frogfishio/homebrew-gritpack/releases/download/bootstrap-v1.2.10/gritpack-1.2.10-aarch64-apple-darwin.tar.gz"
+  version "1.2.10"
+  sha256 "6a18b44fa31fbbd9f0d67ed9012c1c272f04d765cef6be1016aa146a4704c0f9"
   license "LicenseRef-Gritpack-Proprietary"
 
   depends_on arch: :arm64
